b5f0a545f8ad551dcdd30ae203372edaf4b5265c
dipanshu patil <deepanshu.patil69@gmail.com>
2023-04-14 00:21:03 +0530

    redux store set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConf, getGenres } from "./store/slice";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-     
-    </div>
-  )
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    const apiTesting = () => {
+      fetchDataFromApi("/movie/popular").then((res) => {
+          dispatch(getApiConf(res));
+      });
+    };
+    apiTesting();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { store } from "./store/store.js";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import "./index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
